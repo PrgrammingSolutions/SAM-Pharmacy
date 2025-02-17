06546f48685ce48336c3ca15e23b082b155bf208
usman5432 <usmaniking@gmail.com>
2025-02-17 12:26:21 +0500

    Products Added to Table

--- src/Components/AddPurchaseModal.jsx
+++ src/Components/AddPurchaseModal.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const AddPurchaseModal = ({ isOpen, onClose }) => {
+const AddPurchaseModal = ({ isOpen, onClose, onSave }) => {
+  const [purchaseData, setPurchaseData] = useState({
+    itemCode: "",
+    itemName: "",
+    purchaseDate: "",
+    batchNo: "",
+    quantity: "",
+    pack: "",
+    box: "",
+    unitPrice: "",
+    packPrice: "",
+    totalAmount: "",
+  });
+  
   if (!isOpen) return null;
+
+  const handleChange = (e) => {
+    setPurchaseData({ ...purchaseData, [e.target.name]: e.target.value });
+  };
+
+  const handleSave = () => {
+    onSave(purchaseData);
+    onClose();
+  };
 
   return (
     <div 
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 ml-[240px]"
-      onClick={onClose} // Click outside to close
+      onClick={onClose} 
     >
       <div 
         className="bg-white px-8 py-6 rounded-2xl w-[70%] shadow-lg"
-        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
+        onClick={(e) => e.stopPropagation()} 
       >
-        {/* Close Button */}
+
         <div className="flex justify-end">
           <button 
             onClick={onClose} 
@@ -22,31 +44,28 @@ const AddPurchaseModal = ({ isOpen, onClose }) => {
           </button>
         </div>  
 
-        {/* Modal Title */}
         <h2 className="text-lg text-center font-bold text-primary mb-4">
           Add New Purchase
         </h2>
 
-        {/* Form Fields */}
         <div className="grid grid-cols-3 gap-4">
-          <input type="number" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Item Code" />
-          <input type="text" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Item Name" />
-          <input type="date" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Expiry Date" />
+          <input type="number" name="itemCode" onChange={handleChange} className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Item Code" />
+          <input type="text" name="itemName" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Item Name" />
+          <input type="date" name="purchaseDate" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Expiry Date" />
           
-          <input type="text" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Batch No." />
-          <input type="number" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Quantity" />
-          <input type="text" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Pack" />
+          <input type="text" name="batchNo" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Batch No." />
+          <input type="number" name="quantity" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Quantity" />
+          <input type="text" name="pack" onChange={handleChange} className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Pack" />
           
-          <input type="text" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Box" />
-          <input type="number" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Unit Price" />
-          <input type="number" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Pack Price" />
+          <input type="text" name="box" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Box" />
+          <input type="number" name="unitPrice" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Unit Price" />
+          <input type="number" name="packPrice" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Pack Price" />
           
-          <input type="number" className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Total Amount" />
+          <input type="number" name="totalAmount" onChange={handleChange}  className="bg-gray-50 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary" placeholder="Total Amount" />
         </div>
 
-        {/* Submit Button */}
         <div className="flex justify-center mt-6">
-          <button 
+          <button onClick={handleSave}
             className="bg-primary text-white px-6 py-2 rounded-lg font-semibold hover:bg-opacity-90 transition"
           >
             Save Product
--- src/Pages/Purchases/RecordPurchases.jsx
+++ src/Pages/Purchases/RecordPurchases.jsx
@@ -24,66 +24,20 @@ import { Eye } from "lucide-react";
 import AddPurchaseModal from "../../Components/AddPurchaseModal";
 
 const RecordPurchases = () => {
-  const [salesRows, setSalesRows] = useState([
-    {
-      stock_id: "",
-      quantity: "",
-      unit_price: "",
-    },
-  ]);
-  const [medicineData, setMedicineData] = useState([]);
+
   const [patientss, setPatients] = useState([]);
-  const [doctors, setDoctors] = useState([]);
-  const [selectedDoctor, setSelectedDoctor] = useState("");
-  const navigate = useNavigate();
-  const [searchData, setSearchData] = useState("");
-  const [selectedPatient, setSelectedPatient] = useState("");
-
-  const [selectedPatientId, setSelectedPatientId] = useState(null);
-
-  const [saleDate, setSaleDate] = useState(
-    new Date().toISOString().split("T")[0] // Set current date as default
-  );
-
-  const [searchMedicineData, setSearchMedicineData] = useState("");
-  const [selectedMedicine, setSelectedMedicine] = useState("");
-
-  const [selectedMedicineId, setSelectedMedicineId] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [invoiceId, setInvoiceId] = useState(null);
+
   const [isOpen, setIsOpen] = useState(false);
+  const [purchases, setPurchases] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const medicines = await medicineService.fetchAll();
-        setMedicineData(medicines.medicines || []);
+  const handleAddPurchase = (newPurchase) => {
+    setPurchases([...purchases, newPurchase]);
+    setIsOpen(false);
+  };
 
-        const doctors = await doctorService.fetchAllDoctors();
-        setDoctors(doctors.doctors || []);
-      } catch (error) {
-        toast.error("Error fetching data.");
-      }
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const medicines = await medicineService.fetchAll();
-        setMedicineData(medicines.medicines || []);
-
-        const doctors = await doctorService.fetchAllDoctors();
-        setDoctors(doctors.doctors || []);
-      } catch (error) {
-        toast.error("Error fetching data.");
-      }
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
+    useEffect(() => {
     const getPatients = async () => {
       try {
         const response = await patientService.fetchAllPatients();
@@ -95,155 +49,107 @@ const RecordPurchases = () => {
     getPatients();
   }, []);
 
-  const handleRowChange = (index, field, value) => {
-    const updatedRows = [...salesRows];
-    updatedRows[index][field] = value;
-
-    if (field === "stock_id") {
-      const selectedMedicine = medicineData.find((med) => med.id === value);
-      if (selectedMedicine) {
-        updatedRows[index].unit_price = selectedMedicine.discounted_price;
-      }
-    }
-    if (field === "quantity") {
-      if (parseInt(value) <= 0) {
-        toast.error("Quantity must be greater than 0.");
-        return;
-      }
-      const selectedMedicine = medicineData.find(
-        (med) => med.id === updatedRows[index].stock_id
-      );
-
-      if (
-        selectedMedicine &&
-        parseInt(value) > selectedMedicine.quantity_in_stock
-      ) {
-        toast.error("Quantity exceeds available stock.");
-        updatedRows[index].quantity = selectedMedicine.quantity_in_stock;
-      }
-    }
-    setSalesRows(updatedRows);
-  };
-
-  const handleAddRow = () => {
-    setSalesRows([
-      ...salesRows,
-      { stock_id: "", quantity: "", unit_price: "" },
-    ]);
-  };
-
-  const handleRemoveRow = (index) => {
-    setSalesRows(salesRows.filter((_, i) => i !== index));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!selectedPatientId) {
-      toast.error("Please select a patient before submitting.");
-      return;
-    }
-
-    setIsModalOpen(true);
-  };
-
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  const handlePatientSelect = (patient) => {
-    setSelectedPatient(patient);
-    setSelectedPatientId(patient.patient_id);
-    setSearchData("");
-    setPatients((prevData) => ({
-      ...prevData,
-      patient_id: patient.patient_id,
-    }));
-  };
-
-  const handleMedicineSelect = (medicine) => {
-    setSelectedMedicine(medicine);
-    setSearchMedicineData("");
-    setMedicineData((prevData) => ({
-      ...prevData,
-      stock_id: medicine.id,
-    }));
+  const handleRemovePurchase = (index) => {
+    setPurchases(purchases.filter((_, i) => i !== index));
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="w-[90%] m-auto">
+      <form className="w-[90%] m-auto">
         <h1 className="m-[30px] text-center font-[700] text-[20px]">
           Record Medicine Purchases
         </h1>
         <div className="mt-10 pb-4">
           <div className="grid grid-cols-2 gap-3">
             <div className="flex flex-col">
-             <label className="font-semibold text-sm">Distributer Name</label>
-             <input type="text" className="bg-gray-100 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary mt-1" />
+              <label className="font-semibold text-sm">Distributor Name</label>
+              <input
+                type="text"
+                className="bg-gray-100 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary mt-1"
+              />
             </div>
             <div className="flex flex-col">
               <label className="font-semibold text-sm">Invoice Date</label>
-              <input type="text" className="bg-gray-100 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary mt-1" />
+              <input
+                type="text"
+                className="bg-gray-100 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary mt-1"
+              />
             </div>
             <div className="flex flex-col">
               <label className="font-semibold text-sm">Bill Type</label>
-              <input type="text" className="bg-gray-100 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary mt-1" />
+              <input
+                type="text"
+                className="bg-gray-100 px-3 py-2 text-sm border-b-2 rounded-lg focus:outline-none focus:border-primary mt-1"
+              />
             </div>
           </div>
 
-           <hr  className="mt-4"/>
-            <div className="flex justify-end mt-6">
-            <button onClick={()=> setIsOpen(true)} className="bg-white text-primary shadow-[2px_2px_6px_rgba(0,0,0,0.2)] px-8 py-3 rounded-lg font-[600] text-[14px]">
+          <hr className="mt-4" />
+          <div className="flex justify-end mt-6">
+            <button
+              onClick={() => setIsOpen(true)}
+              type="button"
+              className="bg-white text-primary shadow-[2px_2px_6px_rgba(0,0,0,0.2)] px-8 py-3 rounded-lg font-[600] text-[14px]"
+            >
               + Add Medicines
             </button>
-            </div>
-           <div className="mt-4">
-          <table className="w-full border-collapse rounded-lg overflow-hidden shadow-xl shadow-gray-300">
-            <thead>
-              <tr className="bg-primary text-white capitalize leading-normal text-left text-xs">
-                <th className="p-4 w-[10%]">
-                  Purchases Id
-                </th>
-                <th className="p-4 w-[20%]">
-                  Supplier Name
-                </th>
-                <th className="p-4 w-[15%]">
-                  Amount
-                </th>
-                <th className="p-4 w-[15%]">
-                  Purchase Date
-                </th>
-                <th className="p-4 w-[15%] text-center">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              
-                <tr
-                  className="text-xs border-t border-gray-200"
-                >
-                  <td className="p-3 pl-4 w-[10%] font-bold text-primary">
-                    1
-                  </td>
-                  <td className="p-3 w-[20%] font-bold">
-                    Panadol
-                  </td>
-                  <td className="p-3 w-[15%]">
-                    10
-                  </td>
-                  <td className="p-3 w-[15%]">
-                    200
-                  </td>
-                  <td className="p-3 w-[15%] text-center">
-                    <button>
-                      <Eye className="w-5 h-5 text-gray-500 hover:text-primary cursor-pointer" />
-                    </button>
-                  </td>
+          </div>
+
+          {/* Table to show added purchases */}
+          <div className="mt-4">
+            <table className="w-full border-collapse rounded-lg overflow-hidden shadow-xl shadow-gray-300">
+              <thead>
+                <tr className="bg-primary text-white capitalize leading-normal text-left text-xs">
+                <th className="p-2 w-[5%] whitespace-nowrap">Sr No.</th>
+                <th className="p-2 w-[10%] whitespace-nowrap">Item Code</th>
+                <th className="p-2 w-[15%] whitespace-nowrap">Item Name</th>
+                <th className="p-2 w-[12%] whitespace-nowrap">Purchase Date</th>
+                <th className="p-2 w-[10%] whitespace-nowrap">Batch No.</th>
+                <th className="p-2 w-[8%] whitespace-nowrap">Quantity</th>
+                <th className="p-2 w-[8%] whitespace-nowrap">Pack</th>
+                <th className="p-2 w-[8%] whitespace-nowrap">Box</th>
+                <th className="p-2 w-[10%] whitespace-nowrap">Unit Price</th>
+                <th className="p-2 w-[10%] whitespace-nowrap">Pack Price</th>
+                <th className="p-2 w-[10%] whitespace-nowrap">Total Amount</th>
+                <th className="p-2 w-[8%] whitespace-nowrap">Action</th>
                 </tr>
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {purchases.length > 0 ? (
+                  purchases.map((purchase, index) => (
+                    <tr key={index} className="text-xs border-t border-gray-200">
+                      <td className="p-3 pl-4 w-[10%] font-bold text-primary">{index + 1}</td>
+                      <td className="p-3 w-[5%] font-bold">{purchase.itemCode}</td>
+                      <td className="p-3 w-[10%] font-bold">{purchase.itemName}</td>
+                      <td className="p-3 w-[15%]">{purchase.purchaseDate}</td>
+                      <td className="p-3 w-[12%]">{purchase.batchNo}</td>
+                      <td className="p-3 w-[10%]">{purchase.quantity}</td>
+                      <td className="p-3 w-[8%]">{purchase.pack}</td>
+                      <td className="p-3 w-[8%]">{purchase.box}</td>
+                      <td className="p-3 w-[8%]">{purchase.unitPrice}</td>
+                      <td className="p-3 w-[10%]">{purchase.packPrice}</td>
+                      <td className="p-3 w-[10%]">{purchase.totalAmount}</td>
+                      <td className="p-3 w-[10%] text-center">
+                      <IconButton onClick={() => handleRemovePurchase(index)} color="error">
+                          <Remove />
+                        </IconButton>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="12" className="text-center p-4 text-gray-500">
+                      No purchases added yet.
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
 
           <div className="text-center my-[30px]">
             <Button
@@ -254,32 +160,14 @@ const RecordPurchases = () => {
                 "&:hover": { backgroundColor: "#111827" },
               }}
             >
-              Save Medicine Sales
+              Generate Purchase Invoice
             </Button>
           </div>
         </div>
       </form>
-      <MedicineInvoiceModal
-        open={isModalOpen}
-        onClose={closeModal}
-        patientName={selectedPatient?.full_name || ""}
-        doctorName={
-          doctors.find((doc) => doc.id === selectedDoctor)?.name || ""
-        }
-        servicesName={salesRows.map((row) => {
-          const medicine = medicineData.find((med) => med.id === row.stock_id);
-          return medicine ? medicine.medicine_name : "N/A";
-        })}
-        patientId={selectedPatientId}
-        doctorId={selectedDoctor}
-        saleDate={saleDate}
-        salesRows={salesRows}
-        invoiceId={invoiceId}
-      />
-      <AddPurchaseModal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-      />
+
+      {/* Medicine Purchase Modal */}
+      <AddPurchaseModal isOpen={isOpen} onClose={() => setIsOpen(false)} onSave={handleAddPurchase} />
     </>
   );
 };
