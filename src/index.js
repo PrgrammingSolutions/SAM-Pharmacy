68817f71f96f2e2df903c69e46d7552dea94f11d
awais123ahmad <awaisahmadajk@gmail.com>
2025-02-24 13:46:34 +0500

    soemthing

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Toaster />
-    <BrowserRouter>
+    <BrowserRouter basename="/pharmacy">
       <App />
     </BrowserRouter>
   </React.StrictMode>
